ed58d954062803d45e8c8e52a8dda3d5e38474ac
Prelang Builder <builder@prelang.com>
2016-02-02 22:07:55 +0000

    Generated model Alert with migration

---
+++ db/migrate/20160202220755_create_alerts.rb
@@ -0,0 +1,10 @@
+class CreateAlerts < ActiveRecord::Migration
+  def change
+    create_table :alerts do |t|
+      t.references :event, index: true
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
